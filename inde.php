961bcea1e21b0ddf9661a410b49626b443b8dc76
Aji Setiawan <ajiisetiawan09@gmail.com>
2025-01-08 10:50:54 +0700

    mengubah file index.php

--- inde.php
+++ inde.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis nemo dolore ex voluptate repudiandae. Impedit qui consequuntur placeat, culpa veniam recusandae tempora assumenda hic libero, obcaecati, totam exercitationem quaerat fugiat.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
