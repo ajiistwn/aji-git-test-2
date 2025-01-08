598e7f4b877e23ace69887c7a247510c781738e0
Aji Setiawan <ajiisetiawan09@gmail.com>
2025-01-08 10:37:22 +0700

    mengubah file index.php (menambah paragraf)

--- inde.php
+++ inde.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remot 2</title>
 </head>
+
 <body>
-    
+    <h1>Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis nemo dolore ex voluptate repudiandae. Impedit qui consequuntur placeat, culpa veniam recusandae tempora assumenda hic libero, obcaecati, totam exercitationem quaerat fugiat.</p>
 </body>
+
 </html>
